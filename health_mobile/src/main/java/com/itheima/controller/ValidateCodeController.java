b5c0d4a8202f949a116617c1d36f4966704cffb0
linshixiong <17791835203.a>
2023-08-28 21:55:14 +0800

    移动端预约功能

---
+++ health_mobile/src/main/java/com/itheima/controller/ValidateCodeController.java
@@ -0,0 +1,40 @@
+package com.itheima.controller;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.itheima.constant.MessageConstant;
+import com.itheima.constant.RedisConstant;
+import com.itheima.constant.RedisMessageConstant;
+import com.itheima.entity.Result;
+import com.itheima.utils.SMSUtils;
+import com.itheima.utils.ValidateCodeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * 验证码操作
+ */
+@RequestMapping("/validateCode")
+@RestController
+public class ValidateCodeController {
+    @Autowired
+    private JedisPool jedisPool;
+
+    @RequestMapping("/send4Order")
+    public Result send4Order(String telephone){
+        Integer code = ValidateCodeUtils.generateValidateCode(4);
+        try {
+            //未注册阿里云短信服务 暂搁置
+            // SMSUtils.sendShortMessage(SMSUtils.ORDER_NOTICE,telephone,code.toString());
+            System.out.println("code=========="+code);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.SEND_VALIDATECODE_FAIL);
+        }
+        // System.out.println("发送手机验证码为："+code);
+        jedisPool.getResource().setex(telephone+ RedisMessageConstant.SENDTYPE_ORDER,5*60,code.toString());
+        return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
+    }
+
+}
