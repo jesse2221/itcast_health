dbc21958bec89beda76985ff80f1e5062315e476
linshixiong <17791835203.a>
2023-09-13 10:42:49 +0800

    完成运营数据报表

--- health_backend/src/main/java/com/itheima/controller/ReportController.java
+++ health_backend/src/main/java/com/itheima/controller/ReportController.java
@@ -6,9 +6,18 @@ import com.itheima.entity.Result;
 import com.itheima.service.MemberService;
 import com.itheima.service.ReportService;
 import com.itheima.service.SetmealService;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -29,59 +38,134 @@ public class ReportController {
     private ReportService reportService;
 
     @RequestMapping("/getMemberReport")
-    public Result getMemberReport(){
-        Map<String,Object> map = new HashMap<String, Object>();
+    public Result getMemberReport() {
+        Map<String, Object> map = new HashMap<String, Object>();
         List months = new ArrayList();
-        Calendar calendar= Calendar.getInstance();
-        calendar.add(Calendar.MONTH,-12);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, -12);
         for (int i = 0; i < 12; i++) {
-            calendar.add(Calendar.MONTH,1);
+            calendar.add(Calendar.MONTH, 1);
             Date date = calendar.getTime();
             months.add(new SimpleDateFormat("yyyy.MM").format(date));
         }
-        map.put("months",months);
+        map.put("months", months);
 
-        List<Integer> memberCount= null;
+        List<Integer> memberCount = null;
         try {
             memberCount = memberService.findMemberCountByMonth(months);
-            map.put("memberCount",memberCount);
+            map.put("memberCount", memberCount);
         } catch (Exception e) {
             e.printStackTrace();
-            return new Result(false,MessageConstant.GET_MEMBER_NUMBER_REPORT_FAIL);
+            return new Result(false, MessageConstant.GET_MEMBER_NUMBER_REPORT_FAIL);
         }
 
-        return new Result(true, MessageConstant.GET_MEMBER_NUMBER_REPORT_SUCCESS,map);
+        return new Result(true, MessageConstant.GET_MEMBER_NUMBER_REPORT_SUCCESS, map);
     }
 
     @RequestMapping("/getSetmealReport")
-    public Result getSetmealReport(){
+    public Result getSetmealReport() {
         try {
-            List<Map<String,Object>> setmealCount  = setmealService.findSetmealCount();
+            List<Map<String, Object>> setmealCount = setmealService.findSetmealCount();
             Map SetmealReportMap = new HashMap();
-            SetmealReportMap.put("setmealCount",setmealCount);
+            SetmealReportMap.put("setmealCount", setmealCount);
             List setmealNames = new ArrayList();
             for (Map<String, Object> map : setmealCount) {
                 String name = (String) map.get("name");
                 setmealNames.add(name);
             }
-            SetmealReportMap.put("setmealNames",setmealNames);
-            return new Result(true,MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS,SetmealReportMap);
+            SetmealReportMap.put("setmealNames", setmealNames);
+            return new Result(true, MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS, SetmealReportMap);
         } catch (Exception e) {
             e.printStackTrace();
-            return new Result(true,MessageConstant.GET_SETMEAL_COUNT_REPORT_FAIL);
+            return new Result(true, MessageConstant.GET_SETMEAL_COUNT_REPORT_FAIL);
         }
     }
 
     @RequestMapping("/getBusinessReportData")
-    public Result getBusinessReportData(){
+    public Result getBusinessReportData() {
         try {
-            Map<String,Object> map = reportService.getBusinessReportData();
-            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_SUCCESS,map);
+            Map<String, Object> map = reportService.getBusinessReportData();
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS, map);
         } catch (Exception e) {
             e.printStackTrace();
-            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_FAIL);
         }
+    }
 
+    @RequestMapping("/exportBusinessReport")
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response){
+        try {
+            Map<String, Object> result = reportService.getBusinessReportData();
+            //取出返回结果数据，准备将报表数据写入到Excel文件中
+            String reportDate = (String) result.get("reportDate");
+            Integer todayNewMember = (Integer) result.get("todayNewMember");
+            Integer totalMember = (Integer) result.get("totalMember");
+            Integer thisWeekNewMember = (Integer) result.get("thisWeekNewMember");
+            Integer thisMonthNewMember = (Integer) result.get("thisMonthNewMember");
+            Integer todayOrderNumber = (Integer) result.get("todayOrderNumber");
+            Integer thisWeekOrderNumber = (Integer) result.get("thisWeekOrderNumber");
+            Integer thisMonthOrderNumber = (Integer) result.get("thisMonthOrderNumber");
+            Integer todayVisitsNumber = (Integer) result.get("todayVisitsNumber");
+            Integer thisWeekVisitsNumber = (Integer) result.get("thisWeekVisitsNumber");
+            Integer thisMonthVisitsNumber = (Integer) result.get("thisMonthVisitsNumber");
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+
+            //获得Excel模板文件绝对路径
+            String temlateRealPath = request.getSession().getServletContext().getRealPath("template") +
+                    File.separator + "report_template.xlsx";
+
+            //读取模板文件创建Excel表格对象
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(new File(temlateRealPath)));
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            XSSFRow row = sheet.getRow(2);
+            row.getCell(5).setCellValue(reportDate);//日期
+
+            row = sheet.getRow(4);
+            row.getCell(5).setCellValue(todayNewMember);//新增会员数（本日）
+            row.getCell(7).setCellValue(totalMember);//总会员数
+
+            row = sheet.getRow(5);
+            row.getCell(5).setCellValue(thisWeekNewMember);//本周新增会员数
+            row.getCell(7).setCellValue(thisMonthNewMember);//本月新增会员数
+
+            row = sheet.getRow(7);
+            row.getCell(5).setCellValue(todayOrderNumber);//今日预约数
+            row.getCell(7).setCellValue(todayVisitsNumber);//今日到诊数
+
+            row = sheet.getRow(8);
+            row.getCell(5).setCellValue(thisWeekOrderNumber);//本周预约数
+            row.getCell(7).setCellValue(thisWeekVisitsNumber);//本周到诊数
+
+            row = sheet.getRow(9);
+            row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
+            row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
+
+            int rowNum=12;
+            for (Map map : hotSetmeal) {
+                String name = (String) map.get("name");
+                Long setmeal_count = (Long) map.get("setmeal_count");
+                BigDecimal proportion = (BigDecimal) map.get("proportion");
+                row = sheet.getRow(rowNum++);
+                row.getCell(4).setCellValue(name);
+                row.getCell(5).setCellValue(setmeal_count);
+                row.getCell(6).setCellValue(setmeal_count);
+            }
+
+            OutputStream out = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("content-Disposition", "attachment;filename=report.xlsx");
+            workbook.write(out);
+            out.flush();
+            out.close();
+            workbook.close();
+
+
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
     }
 
 }
